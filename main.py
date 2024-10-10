d4dcef863d7e87b28254ce81e31593eb5f7beba8
Nestor <nestorzamili@gmail.com>
2024-10-10 15:05:20 +0700

    Refactor main.py to use environment variables for API configuration and implement logging

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import os
 import requests
-from telegram.ext import Updater, MessageHandler, Filters
+import logging
+from telegram import Update
+from telegram.ext import Updater, MessageHandler, Filters, CallbackContext
 
+# Konfigurasi logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Mengambil variabel dari environment
 TOKEN = os.getenv('BOT_TOKEN')
 API_KEY = os.getenv('API_KEY')
 IDS = os.getenv('IDS')
-API_URL = os.getenv('API_URL')
+API_URL = os.getenv('API_URL')  # URL API
 
 def trigger_api(message):
+    """Memicu API dengan pesan yang diterima."""
     headers = {
         'x-api-key': API_KEY,
         'ids': IDS,
@@ -19,18 +28,29 @@ def trigger_api(message):
         return response.text
     return f"Error: {response.status_code}"
 
-def handle_message(update, context):
+def handle_message(update: Update, context: CallbackContext):
+    """Menangani pesan yang masuk dan meneruskannya ke API."""
     user_message = update.message.text
+    logger.info(f"Pesan diterima: {user_message}")  # Tambahkan logging
     api_response = trigger_api(user_message)
     update.message.reply_text(f"Pesan kamu telah diteruskan. API Response: {api_response}")
 
 def main():
+    """Fungsi utama untuk menjalankan bot."""
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
 
+    # Menangani semua pesan teks
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
-    updater.start_polling()
+    # Mengatur webhook
+    PORT = int(os.environ.get('PORT', 5000))
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN)
+    updater.bot.setWebhook(f'https://bot-telegram-samunu-6b58d8079dd0.herokuapp.com/{TOKEN}')
+
+    # Menjalankan bot
     updater.idle()
 
 if __name__ == '__main__':
